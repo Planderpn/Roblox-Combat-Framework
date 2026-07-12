bb3820a13a3bd640d1209a89ce07e172b870b744
Planderpn <134508517+Planderpn@users.noreply.github.com>
2026-07-12 18:00:04 +0800

    Updated Hitbox.lua
    Create hitbox module

--- src/Hitbox.lua
+++ src/Hitbox.lua
@@ -1,3 +1,18 @@
-local Module = {}
+local Hitbox = {}
 
-return Module
+function Hitbox:Create(position, size)
+	local part = Instance.new("Part")
+	part.Size = size or Vector3.new(4, 4, 4)
+	part.CFrame = position
+	part.Anchored = true
+	part.CanCollide = false
+	part.Transparency = 1
+	part.Name = "Hitbox"
+	part.Parent = workspace
+
+	game:GetService("Debris"):AddItem(part, 0.1)
+
+	return part
+end
+
+return Hitbox
