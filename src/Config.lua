a210ce77fc3517bd67645fcc28b7b68efb38f8fa
Planderpn <134508517+Planderpn@users.noreply.github.com>
2026-07-12 17:57:23 +0800

    Config.lua

--- src/Config.lua
+++ src/Config.lua
@@ -1,3 +1,32 @@
-local Module = {}
+local Config = {}
 
-return Module
+Config.Combo = {
+	MaxHits = 4,
+	ResetTime = 1.2,
+}
+
+Config.Damage = {
+	Light = 8,
+	Heavy = 15,
+}
+
+Config.Stun = {
+	Duration = 0.5,
+}
+
+Config.Dash = {
+	Distance = 16,
+	Cooldown = 1,
+}
+
+Config.Block = {
+	DamageReduction = 0.75,
+}
+
+Config.Knockback = {
+	Force = 30,
+}
+
+return Config
+
+Add combat configuration
