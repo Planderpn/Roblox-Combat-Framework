804181be3989eac69a417322e5e662ffe1861841
Planderpn <134508517+Planderpn@users.noreply.github.com>
2026-07-12 17:59:39 +0800

    Updated Damage.lua
    Create damage module

--- src/Damage.lua
+++ src/Damage.lua
@@ -1,3 +1,21 @@
-local Module = {}
+local Config = require(script.Parent.Config)
 
-return Module
+local Damage = {}
+
+function Damage:Deal(attacker, target, amount)
+	if not target then
+		return
+	end
+
+	local humanoid = target:FindFirstChildOfClass("Humanoid")
+
+	if not humanoid then
+		return
+	end
+
+	amount = amount or Config.Damage.Light
+
+	humanoid:TakeDamage(amount)
+end
+
+return Damage
